749eedf2b60f92af0ca1cae1d1953e6ce363a473
ryou.ozaki <ryo244fighting@docomo.ne.jp>
2020-11-28 15:10:07 +0900

    Initial Commit

---
+++ HelloWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloWord
+//
+//  Created by 尾崎玲 on 2020/11/28.
+//  Copyright © 2020 ryo.ozaki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
